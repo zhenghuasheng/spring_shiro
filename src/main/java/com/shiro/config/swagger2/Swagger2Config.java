4a1cecce6d4f228aa00301766ba4d8099bdc2735
zhenghuasheng <zhenghuasheng@aixuedai.com>
2017-12-13 15:27:31 +0800

    update

---
+++ src/main/java/com/shiro/config/swagger2/Swagger2Config.java
@@ -0,0 +1,44 @@
+package com.shiro.config.swagger2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * http://localhost:8090/swagger-ui.html
+ * @author zhenghuasheng
+ * @date 2017/11/8.11:07
+ */
+@Configuration
+@EnableWebMvc
+@EnableSwagger2
+@ComponentScan(basePackages = {"com.shiro.controller"})
+public class Swagger2Config {
+
+    @Bean
+    public Docket createRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.shiro.controller"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Spring Boot中使用Swagger2构建RESTful APIs")
+                .description("更多Spring Boot相关文章请关注：http://blog.didispace.com/")
+                .termsOfServiceUrl("http://localhost:9999/")
+                .contact("zhs")
+                .version("1.0")
+                .build();
+    }
+}
